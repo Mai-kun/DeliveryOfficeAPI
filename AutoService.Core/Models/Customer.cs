49b62bc106f45cdab2bc931f4cce670f3f05ea64
Mai-kun <amaz1ngcreature10@mail.ru>
2024-11-30 20:54:46 +0300

    Удалил старое

--- AutoService.Core/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoService.Core.Models;
-
-public class Customer
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string? Phone { get; set; }
-    public List<Car> Cars { get; set; } = null!;
-}
