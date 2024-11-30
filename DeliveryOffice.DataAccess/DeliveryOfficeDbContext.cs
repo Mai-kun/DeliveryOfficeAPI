c4333369922b7bc3d9beea8bc4d70166f72d175e
Mai-kun <amaz1ngcreature10@mail.ru>
2024-11-30 20:56:09 +0300

    Написал, запустил миграции и бд

---
+++ DeliveryOffice.DataAccess/DeliveryOfficeDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DeliveryOffice.Core.Configurations;
+using DeliveryOffice.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryOffice.DataAccess;
+
+public class DeliveryOfficeDbContext : DbContext
+{
+    /// <remarks>
+    ///     dotnet tool install --global dotnet-ef
+    ///     dotnet ef migrations add InitialCreate --project DeliveryOffice.DataAccess\DeliveryOffice.DataAccess.csproj
+    ///     dotnet ef database update --project DeliveryOffice.DataAccess\DeliveryOffice.DataAccess.csproj
+    /// </remarks>
+    public DeliveryOfficeDbContext(DbContextOptions<DeliveryOfficeDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ConfigurationAnchor).Assembly);
+
+    public DbSet<Bill> Bills { get; set; } = null!;
+    public DbSet<Buyer> Buyers { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Supplier> Suppliers { get; set; } = null!;
+}
